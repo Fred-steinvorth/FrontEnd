e7ac796735fdfb379a09518ab7c96b3cc19f68a0
Oscar <oscar.espinozaea19@gmail.com>
2023-04-18 23:47:53 -0600

    InitLogicHttpClient

--- LoginFlow/Views/LoginPage.xaml.cs
+++ LoginFlow/Views/LoginPage.xaml.cs
@@ -1,10 +1,20 @@
+using LoginFlow.Models;
+using LoginFlow.Models.Request;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json;
+
 namespace LoginFlow.Views;
 
-public partial class LoginPage : ContentPage
-{
+public partial class LoginPage : ContentPage{
+
+    HttpClient client;
+    JsonSerializerOptions _jsonSerializerOptions;
     public LoginPage()
     {
         InitializeComponent();
+        client = new HttpClient();
+        _jsonSerializerOptions = new JsonSerializerOptions();
     }
 
     protected override bool OnBackButtonPressed()
@@ -15,7 +25,29 @@ public partial class LoginPage : ContentPage
 
     private async void LoginButton_Clicked(object sender, EventArgs e)
     {
-        if (IsCredentialCorrect(Username.Text, Password.Text))
+        IsCredentialCorrect(Username.Text, Password.Text);
+    }
+
+    async void IsCredentialCorrect(string username, string password)
+    {
+        String url = "https://localhost:44353/api/login";
+        ReqLoginUser req = new ReqLoginUser();
+        req.user = new Usuario();
+
+        req.user.email = username;
+        req.user.password = password;
+        req.session = "1234";
+
+        String json = JsonConvert.SerializeObject(req);
+        StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+        Console.WriteLine(content);
+        ResLoginUser res = new ResLoginUser();
+
+        var responseApi = await client.PostAsync(url, content);
+        var respuestaNormal = await responseApi.Content.ReadAsStringAsync();
+        res = System.Text.Json.JsonSerializer.Deserialize<ResLoginUser>(respuestaNormal);
+
+        if (res.result)
         {
             await SecureStorage.SetAsync("hasAuth", "true");
             await Shell.Current.GoToAsync("///home");
@@ -24,11 +56,5 @@ public partial class LoginPage : ContentPage
         {
             await DisplayAlert("Login failed", "Uusername or password if invalid", "Try again");
         }
-    }
-
-
-    bool IsCredentialCorrect(string username, string password)
-    {
-        return Username.Text == "admin" && Password.Text == "1234";
     }
 }
--- LoginFlow/Views/RecipeMaintenancePage.xaml.cs
+++ LoginFlow/Views/RecipeMaintenancePage.xaml.cs
@@ -1,45 +1,27 @@
-using System;
-using System.Collections.Generic;
-using LoginFlow.Models;
-using Microsoft.Maui.Controls;
+using LoginFlow.Models.Request;
+using Newtonsoft.Json;
 
 namespace LoginFlow.Views
 {
     public partial class RecipeMaintenancePage : ContentPage
     {
-        public List<Recipe> Recipes { get; set; }
-
+        HttpClient client;
         public RecipeMaintenancePage()
         {
+            client = new HttpClient();
             InitializeComponent();
+            LoadRecipeAsync();
+        }
 
-            // Creamos la lista de recetas
-            Recipes = new List<Recipe>();
-
-            // Creamos una nueva receta y le asignamos valores a sus propiedades
-            var recipe1 = new Recipe
-            {
-                Name = "Ensalada de tomate",
-                Ingredients = "Tomates, cebolla, aceite de oliva, vinagre balsámico, sal, pimienta",
-                Steps = "1. Cortar los tomates y la cebolla en rodajas\n2. Mezclar el aceite de oliva, el vinagre balsámico, la sal y la pimienta en un bol\n3. Colocar las rodajas de tomate y cebolla en un plato y rociar la mezcla de aceite y vinagre por encima"
-            };
-
-            // Agregamos la receta a la lista
-            Recipes.Add(recipe1);
-
-            // Agregamos más recetas de la misma forma
-            var recipe2 = new Recipe
-            {
-                Name = "Arroz con pollo",
-                Ingredients = "Arroz, pollo, cebolla, ajo, pimiento, guisantes, zanahoria, aceite, sal, pimienta, caldo de pollo",
-                Steps = "1. Dorar el pollo en una olla con aceite\n2. Agregar la cebolla, ajo, pimiento, guisantes y zanahoria picados y saltear\n3. Agregar el arroz y saltear\n4. Agregar el caldo de pollo y dejar cocinar hasta que el arroz esté listo"
-            };
-
-            Recipes.Add(recipe2);
-
-            // Asignamos la lista de recetas a la ListView mediante el atributo ItemsSource en XAML
-            ListView.ItemsSource = Recipes;
-
+        public async void LoadRecipeAsync()
+        {
+            String url = "https://localhost:44353/api/receta";
+            ReqObtenerTodasLasRecetas req = new ReqObtenerTodasLasRecetas();
+            ResObtenerTodasLasRecetas res = new ResObtenerTodasLasRecetas();
+            req.session = "12345";
+            var responseApi = await client.GetStringAsync(url);
+            res = JsonConvert.DeserializeObject<ResObtenerTodasLasRecetas>(responseApi);
+            ListView.ItemsSource = res.listaDeRecetas;
         }
     }
 }
