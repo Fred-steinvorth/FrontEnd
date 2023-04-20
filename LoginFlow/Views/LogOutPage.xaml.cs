1c60375e0bb8e52818e01217794922cf6a63e49b
Oscar <oscar.espinozaea19@gmail.com>
2023-04-19 21:01:28 -0600

    creatGloblaUser

--- LoginFlow/Views/LogOutPage.xaml.cs
+++ LoginFlow/Views/LogOutPage.xaml.cs
@@ -4,12 +4,16 @@ public partial class LogOutPage : ContentPage
 {
 	public LogOutPage()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+        String username = LoginPage.usuarioGlobal.username;
+        TextDear.Text = "Dear " + username;
+    }
 
 	private async void LogoutButton_Clicked(object sender, EventArgs e)
 	{
-		if (await DisplayAlert("Are you sure?", "You will be logged out.", "Yes", "No"))
+        
+        
+        if (await DisplayAlert("Are you sure?", "You will be logged out.", "Yes", "No"))
 		{
 			SecureStorage.RemoveAll();
 			await Shell.Current.GoToAsync("///login");
